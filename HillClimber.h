3279fd1fcb5ec0349531352205403e8467c75204
Emilio Singh <u14006512@tuks.co.za>
2016-07-01 14:43:06 +0200

    Optimisation Strategies Hierarchy
    I have added the H files pertaining to the optimisation strategy hierarchy

---
+++ HillClimber.h
@@ -0,0 +1,37 @@
+/**
+ * @class HillClimber
+ *
+ * The SwarmVis system must be able to perform other optimisation strategies beyond the PSO mechanism.
+ * Hill Climbing is one such process that can be performed using a swarm as part of the hill climbing strategy.
+ * The HillClimber class therefore is used to encapsulate the search process.
+ *
+ * @note Still in Progress
+ *
+ * @author Emilio Singh u14006512
+ *
+ * @version 0.1
+ *
+ * @date: 2016/07/01 12:16
+ *
+ * Contact: u14006512@tuks.co.za
+ *
+ * */
+
+#ifndef OPT_HILLCLIMBER_H
+#define OPT_HILLCLIMBER_H
+#include "OTP_Process.h"
+
+class HillClimber:public OTP_Process {
+
+public:
+
+    ///The overrid method for solve
+    virtual void solve() override;
+
+    HillClimber() { }
+
+    virtual ~HillClimber() { }
+};
+
+
+#endif //OPT_HILLCLIMBER_H
