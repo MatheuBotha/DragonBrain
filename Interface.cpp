2ddafc8f0d40038b4a3d1ca7c2c4e9caed3edcd6
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-07-03 02:22:40 +0200

    These files got lost in a previous commit?

---
+++ Interface.cpp
@@ -0,0 +1,19 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+//    engine.rootContext()->setContextProperty("mather", math);
+
+
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+
+    return app.exec();
+}
