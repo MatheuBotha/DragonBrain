769ec148245a9a39fd017db1a8f00c7ea1e39ed2
GerardVanWyk <u14101263@tuks.co.za>
2016-07-11 16:30:15 +0200

    Snapshot object class

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Drareg on 2016-07-11.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "spplebeez" << std::endl;
+
+    return 0;
+}
---
+++ snapshot.h
@@ -0,0 +1,15 @@
+//
+// Created by Gerard van wyk u14101263 on 2016-07-11.
+//
+
+#ifndef DRAGONBRAIN_SNAPSHOT_H
+#define DRAGONBRAIN_SNAPSHOT_H
+
+class Snapshot {
+private:
+
+public:
+    Snapshot(Particle *ParticleList, int **ParticleLinks);
+};
+
+#endif //DRAGONBRAIN_SNAPSHOT_H
