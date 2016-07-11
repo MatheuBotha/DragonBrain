60ca63c80dc6cb708bb5bb7e546bd64c63b762f9
GerardVanWyk <u14101263@tuks.co.za>
2016-07-11 20:12:37 +0200

    Corrected comment

--- snapshot.cpp
+++ snapshot.cpp
@@ -5,7 +5,7 @@
  *
  * The snapshot is -as the name suggests- a snapshot of the particle positions of the optimiser in the current epoch.
  * A snapshot consists of a 1D array of Particles and a 2D array of integers describing the connection topology of the particles.
- * Snapshots are stored in a queue in the snapshot manager, and are created the generic optimiser class (OPT).
+ * Snapshots are stored in a queue in the snapshot manager, and are created by the generic optimiser class (OPT).
  * The sequence of snapshots has multiple uses, primarily to be used the graphics system to visualize the locations of
  * the particles.
  *
