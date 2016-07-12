68d12f4b5c9f1cfd60088c9f5c8ecf366b18e6e2
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-07-12 21:32:59 +0200

    Commented code with doxygen compliant comments
    All SettingsPackages now commented

--- SettingsPackage/optimizersettingspackage.h
+++ SettingsPackage/optimizersettingspackage.h
@@ -3,6 +3,13 @@
 
 #include <string>
 
+/**
+ * @class OptimizerSettingsPackage
+ *
+ * This class is a wrapper for all the settings related to the Optimizer
+ * The contents are rather simplistic, only getters and setters.
+ * */
+
 class OptimizerSettingsPackage
 {
 private:
@@ -11,16 +18,59 @@ private:
     double cognitiveCoefficient;
     double socialCoefficient;
 public:
+    /**
+     * @brief The default constructor for an OptimizerSettingsPackage
+     */
     OptimizerSettingsPackage();
+    /**
+     * @brief Creates a  OptimizerSettingsPackage with specified settings (currently unused)
+     * @param particlePlacement (string pointer)
+     * @param inertiaWeight (double)
+     * @param cognitiveCoefficient (double)
+     * @param socialCoefficient (double)
+     */
     OptimizerSettingsPackage(std::string*, double, double, double);
+
+    /**
+     * @brief
+     * @return Returns the string containing the user specified particle placement (or null)
+     */
     std::string* getParticlePlacementString();
+    /**
+     * @brief
+     * @return Returns a double containing the inertia weight
+     */
     double getInertiaWeight();
+    /**
+     * @brief
+     * @return Returns a double containing the cognitive coefficient
+     */
     double getCognitiveCoefficient();
+    /**
+     * @brief
+     * @return Returns a double containing the social coefficient
+     */
     double getSocialCoefficient();
 
+    /**
+     * @brief Sets the Particle Placement
+     * @param ParticlePlacement (string pointer)
+     */
     void setParticlePlacementString(std::string*);
+    /**
+     * @brief Sets the Inertia Weight
+     * @param InertiaWeight (double)
+     */
     void setInertiaWeight(double);
+    /**
+     * @brief Sets the Cognitive Coefficient
+     * @param cognitiveCoefficient (double)
+     */
     void setCognitiveCoefficient(double);
+    /**
+     * @brief Sets the Social Coefficient
+     * @param socialCoefficient (double)
+     */
     void setSocialCoefficient(double);
 };
 
--- SettingsPackage/settingspackage.h
+++ SettingsPackage/settingspackage.h
@@ -8,6 +8,15 @@
 #include "graphicssettingspackage.h"
 #include "optimizersettingspackage.h"
 #include "problemdomainsettingspackage.h"
+
+/**
+ * @class ProblemDomainSettingsPackage
+ *
+ * This class is a general SettingsPackage which contains
+ * other, more specific SettingsPackages and extracts settings
+ * from the GUI
+ * */
+
 class SettingsPackage: public QObject
 {
     Q_OBJECT
@@ -20,15 +29,45 @@ private:
 
 
 public:
+    /**
+     * @brief Default SettingsPackage Constructor, calls default constructors for a
+     * Graphics, Optimizer and ProblemDomain SettingsPackage.
+     */
     SettingsPackage();
 
+    /**
+     * @brief
+     * @return Returns the associated GraphicsSettingsPackage
+     */
     GraphicsSettingsPackage* getGraphicsSettingsPackage();
+    /**
+     * @brief
+     * @return Returns the associated OptimizerSettingsPackage
+     */
+
     OptimizerSettingsPackage* getOptimizerSettingsPackage();
+    /**
+     * @brief
+     * @return Returns the associated ProblemDomainSettingsPackage
+     */
     ProblemDomainSettingsPackage* getProblemDomainSettingsPackage();
 
+    /**
+      * @brief Sets the swarm size (given by GUI)
+      * @param SwarmSize (int)
+      */
     Q_INVOKABLE void generateSettingsGeneral(int);
+    /**
+      * @brief Generates a GraphicsSettingsPackage using input from the GUI
+      */
     Q_INVOKABLE void generateSettingsGraphics(QString, int, bool, bool, int);
+    /**
+      * @brief Generates a OptimizerSettingsPackage using input from the GUI
+      */
     Q_INVOKABLE void generateSettingsOptimizer(QString*, double, double, double);
+    /**
+      * @brief Generates a ProblemDomainSettingsPackage using input from the GUI
+      */
     Q_INVOKABLE void generateSettingsDomain(QString, int, int, int, int, int, double, double, double);
 };
 
