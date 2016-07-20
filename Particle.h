9df287358789229774489615660a497b918566cc
Emilio Singh <u14006512@tuks.co.za>
2016-07-20 15:02:39 +0200

    Particle Revision
    Additional member variables have been added to the Particle class

--- Particle.h
+++ Particle.h
@@ -36,7 +36,8 @@ public:
         Particle::velocity = velocity;
     }
 
-    void setVelocitybyNumber(double v){
+    ///Alternative setter for velocity
+    void setVelocity(double v){
         *velocity=v;
     }
 
@@ -102,6 +103,7 @@ public:
     Particle() {
         velocity=new double(0.0);
         positionArray=new double[2];
+        personalBestPositions=new double[2];
     }
 
     /** @brief Advanced Constructor Method
@@ -129,6 +131,7 @@ public:
             positionArray[i]=pos[i];
         }
 
+        personalBestPositions=new double[2];
     }
     ///Getter for the fitness value
     double getFitnessValue() const {
@@ -149,14 +152,48 @@ public:
     void setFitnessValue(double fitnessValue) {
         Particle::fitnessValue = fitnessValue;
     }
+
+    ///Getter for the best personal positions array
+    double *getPersonalBestPositions() const {
+        return personalBestPositions;
+    }
+
+    ///Setter for the best personal positions array
+    void setPersonalBestPositions(double *personalBestPositions) {
+        Particle::personalBestPositions = personalBestPositions;
+    }
+
+    ///Getter for the neighbourhood best
+    double getNeighbourhoodBest() const {
+        return neighbourhoodBest;
+    }
+
+    ///Setter for the neighbourhood best
+    void setNeighbourhoodBest(double neighbourhoodBest) {
+        Particle::neighbourhoodBest = neighbourhoodBest;
+    }
+
+    ///Getter for the neighbourhood best position array
+    double *getNeighbourhoodBestPosition() const {
+        return neighbourhoodBestPosition;
+    }
+
+    ///Setter for the neighbourhood best position array
+    void setNeighbourhoodBestPosition(double *neighbourhoodBestPosition) {
+        Particle::neighbourhoodBestPosition = neighbourhoodBestPosition;
+    }
+
 private:
 
 
     double * positionArray; ///> the array of double values holding the positions per dimension of the particle
-
+    double * personalBestPositions; ///>the array that holds the positions of the best personal state of the particle.
     double * velocity; ///> the velocity of the particle
+    double * neighbourhoodBestPosition; ///> The position of the best neighbour
     double fitnessValue=0; ///> The fitness value of the particle in relation to an objective function
     double personalBest=0; ///> The personal best or memory of each particle
+    double neighbourhoodBest=0; ///> The fitness value associated with the best neighbour
+
 };
 
 
