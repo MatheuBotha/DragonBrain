f75ef13ec6ec964c6f0f14a7eedb7246bfec9e4b
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-07-21 23:24:53 +0200

    Restructured file structure. Included unit testing
    Split into separate folders for src and test, unit tests appear fully
    functional using CMake and CLion (apologies for big commit)

---
+++ SettingsPackage/src/settingspackage.cpp
@@ -0,0 +1,67 @@
+#include "settingspackage.h"
+#include <iostream>
+
+
+SettingsPackage::SettingsPackage()
+{
+    gpPkg = new GraphicsSettingsPackage();
+    optPkg = new OptimizerSettingsPackage();
+    probPkg = new ProblemDomainSettingsPackage();
+}
+
+int SettingsPackage::getSwarmSize()
+{
+    return swarmSize;
+}
+
+
+void SettingsPackage::generateSettingsGeneral(int sSize)
+{
+    swarmSize = sSize;
+}
+
+GraphicsSettingsPackage* SettingsPackage::getGraphicsSettingsPackage()
+{
+    return gpPkg;
+}
+
+OptimizerSettingsPackage* SettingsPackage::getOptimizerSettingsPackage()
+{
+    return optPkg;
+}
+
+ProblemDomainSettingsPackage* SettingsPackage::getProblemDomainSettingsPackage()
+{
+    return probPkg;
+}
+
+
+// Some manual labour to be done here, nothing special at all
+void SettingsPackage::generateSettingsGraphics(std::string resolution, int renderSpeed, bool showLinks, bool showPath, int maxRam)
+{
+    gpPkg->setMaxRam(maxRam);
+    gpPkg->setRenderSpeed(renderSpeed);
+    gpPkg->setShowLinks(showLinks);
+    gpPkg->setShowPath(showPath);
+}
+
+void SettingsPackage::generateSettingsOptimizer(std::string particlePlacement, double InertiaWeight, double cognitiveCoefficient, double socialCoefficient, int maxIterations, int cutoffAcc)
+{
+    optPkg->setParticlePlacementString(particlePlacement);
+    optPkg->setInertiaWeight(InertiaWeight);
+    optPkg->setCognitiveCoefficient(cognitiveCoefficient);
+    optPkg->setSocialCoefficient(socialCoefficient);
+    optPkg->setMaxIterations(maxIterations);
+    optPkg->setCutoffAcc(cutoffAcc);
+}
+
+void SettingsPackage::generateSettingsDomain(std::string objFunction, int dimensions, int x1_min, int x1_max, int x2_min, int x2_max, double TransformationA, double TransformationB, double TransformationC)
+{
+    probPkg->setObjectiveFunction(objFunction);
+    probPkg->setDimensions(dimensions);
+    int boundaries[4] = {x1_min, x1_max, x2_min, x2_max};
+    probPkg->setBoundaries(boundaries);
+    double transformations[3] = {TransformationA, TransformationB, TransformationC};
+    probPkg->setTransformations(transformations);
+}
+
