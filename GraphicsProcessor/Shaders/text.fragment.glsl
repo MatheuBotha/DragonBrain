be3b768f4a8f1e26e801ca2befed10f94f985eac
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-10-22 20:42:56 +0200

    Adjusted for compatibility with earlier GL version.

--- GraphicsProcessor/Shaders/text.fragment.glsl
+++ GraphicsProcessor/Shaders/text.fragment.glsl
@@ -1,12 +1,12 @@
-#version 330 core
-in vec2 TexCoords;
-out vec4 color;
+#version 300 es
+in mediump vec2 TexCoords;
+out mediump vec4 color;
 
-uniform sampler2D text;
-uniform vec3 textColor;
+uniform mediump sampler2D text;
+uniform mediump vec3 textColor;
 
 void main()
 {
-    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
+    mediump vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
     color = vec4(textColor, 1.0) * sampled;
 }
--- GraphicsProcessor/Shaders/text.vertex.glsl
+++ GraphicsProcessor/Shaders/text.vertex.glsl
@@ -1,6 +1,6 @@
-#version 330 core
-layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
-out vec2 TexCoords;
+#version 300 es
+layout (location = 0) in mediump vec4 vertex; // <vec2 pos, vec2 tex>
+out mediump vec2 TexCoords;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -8,6 +8,7 @@ uniform mat4 projection;
 
 void main()
 {
+
     gl_Position = projection * view * model * vec4(vertex.xy, 0.0, 1.0);
     TexCoords = vertex.zw;
 }
